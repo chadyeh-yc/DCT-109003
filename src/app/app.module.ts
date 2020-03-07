b32e53569b7666e26021d5571dfa42ef941a96ff
ChadYeh <chadyeh.yc@outlook.com>
2020-03-07 11:52:48 +0800

    Add /tables, /charts, /utilities/colors, NotFoundComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Page1Component } from './page1/page1.component';
-import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TablesComponent } from './tables/tables.component';
+import { ChartsComponent } from './charts/charts.component';
+import { ColorsComponent } from './utilities/colors/colors.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component,
-    Page2Component,
-    DashboardComponent
+    DashboardComponent,
+    TablesComponent,
+    ChartsComponent,
+    ColorsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
