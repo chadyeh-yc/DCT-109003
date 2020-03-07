7d8e70465a852e50e3cc55e388bab6da92bd157c
ChadYeh <chadyeh.yc@outlook.com>
2020-03-07 14:20:26 +0800

    Modify New Component Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { TablesComponent } from './tables/tables.component';
+import { ChartsComponent } from './charts/charts.component';
+import { ColorsComponent } from './utilities/colors/colors.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'tables', component: TablesComponent },
+  { path: 'charts', component: ChartsComponent },
+  {
+    path: 'utilities',
+    children: [
+      { path: 'colors', component: ColorsComponent }
+    ]
+  },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { TablesComponent } from './tables/tables.component';
 import { ChartsComponent } from './charts/charts.component';
 import { ColorsComponent } from './utilities/colors/colors.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
+    NotFoundComponent,
     TablesComponent,
     ChartsComponent,
-    ColorsComponent,
-    NotFoundComponent
+    ColorsComponent
   ],
   imports: [
     BrowserModule,
