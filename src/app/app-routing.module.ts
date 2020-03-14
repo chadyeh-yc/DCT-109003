c3519f9dfa8765e18a2ac8177696ccf09232c529
ChadYeh <chadyeh.yc@outlook.com>
2020-03-14 09:32:49 +0800

    Add layout login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { TablesComponent } from './tables/tables.component';
 import { ChartsComponent } from './charts/charts.component';
+import { ColorsComponent } from './utilities/colors/colors.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'tables', component: TablesComponent },
-  { path: 'charts', component: ChartsComponent },
-  { path: 'utilities', loadChildren: () => import('./utilities/utilities.module').then(m => m.UtilitiesModule) },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'tables', component: TablesComponent,
+        canActivate: [AuthGuard] },
+      { path: 'charts', component: ChartsComponent, data: { title: '圖表' } },
+      { path: 'charts/:type', component: ChartsComponent, data: { title: '圖表' } },
+      { path: 'utilities',
+        loadChildren: () => import('./utilities/utilities.module').then(m => m.UtilitiesModule) },
+    ]
+  },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true
+    useHash: true,
+    onSameUrlNavigation: 'reload'
   })],
   exports: [RouterModule]
 })
--- src/app/utilities/utilities-routing.module.ts
+++ src/app/utilities/utilities-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ColorsComponent } from './colors/colors.component';
 
-
-const routes: Routes = [{ path: 'colors', component: ColorsComponent }];
+const routes: Routes = [
+  { path: 'colors', component: ColorsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
